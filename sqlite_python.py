c33d7d9d96ae77aad9210760322ea07a7f9cb251
Boris Skip <boris.v.skip@gmail>
2019-09-29 21:01:02 +0300

    Removing unnecessary files and added sql tutorial codes

--- sqlite_python.py
+++ sqlite_python.py
@@ -1,35 +1,52 @@
 import sqlite3
 
-connection = sqlite3.connect("company.db")
-
+connection = sqlite3.connect("chinook.db")
 cursor = connection.cursor()
 
 # delete
 #cursor.execute("""DROP TABLE employee;""")
 
-
 #Create SQL tables in Python
+
+# https://www.sqlitetutorial.net/sqlite-join/
+# https://www.sqlitetutorial.net/sqlite-inner-join/
+# https://www.sqlitetutorial.net/sqlite-cross-join/
+# https://www.sqlitetutorial.net/sqlite-full-outer-join/
+# https://www.sqlitetutorial.net/sqlite-self-join/
+# https://www.sqlitetutorial.net/sqlite-union/
+# https://www.sqlitetutorial.net/sqlite-count-function/
+
 sql_command = """
-CREATE TABLE employee ( 
-staff_number INTEGER PRIMARY KEY, 
-fname VARCHAR(20), 
-lname VARCHAR(30), 
-gender CHAR(1), 
-joining DATE,
-birth_date DATE);"""
-
+SELECT
+    trackid,
+    name,
+    tracks.albumid AS album_id_tracks,
+    albums.albumid AS album_id_albums,
+    title
+FROM
+    tracks
+    INNER JOIN albums ON albums.albumid = tracks.albumid;
+"""
 cursor.execute(sql_command)
+print("fetchall:")
+res = cursor.fetchall()
+for r in res:
+    print(r)
 
-sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
-    VALUES (NULL, "William", "Shakespeare", "m", "1961-10-25");"""
-cursor.execute(sql_command)
-
-
-sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
-    VALUES (NULL, "Frank", "Schiller", "m", "1955-08-17");"""
-cursor.execute(sql_command)
+cursor.execute('''
+SELECT
+   albumid, COUNT(*)
+FROM
+   tracks
+GROUP BY
+   albumid
+HAVING COUNT(*) > 25
+''')
+print("fetchall:")
+albums = cursor.fetchall()
+for album in albums:
+    print(album)
 
 # never forget this, if you want the changes to be saved:
 connection.commit()
-
 connection.close()
