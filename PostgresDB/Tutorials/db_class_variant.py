5974c22a9bfc8e34b643ebc515fa20bb73b95ced
Boris Skip <boris.v.skip@gmail.com>
2019-10-10 10:02:39 +0300

    Before SS tests

--- PostgresDB/Tutorials/db_class_variant.py
+++ PostgresDB/Tutorials/db_class_variant.py
@@ -1,5 +1,14 @@
+import config
 import psycopg2
 
+# The structure of config.py file
+# db_config = {
+#             'host': "localhost",                 # database host
+#             'port': port,                        # port
+#             'user': "localuser",                 # username
+#             'password': "sequre_pass",           # password
+#             'database': "library",               # database
+# }
 
 class DB(object):
     def __init__(self, config):
@@ -9,22 +18,39 @@ class DB(object):
     def __del__(self):
         self.conn.close()
 
-    def query(self, sql):
+    def get_data(self, sql):
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql)
+            self.conn.commit()
+            return cursor.fetchall()
+
+    def count_frequency(self, sql):
         with self.conn.cursor() as cursor:
             cursor.execute(sql)
             self.conn.commit()
             return cursor.fetchall()
 
 
-db_config = {
-            'host': "127.0.0.1",                 # database host
-            'port': 5432,                        # port
-            'user': "postgres",                  # username
-            'password': "SBoris",                # password
-            'database': "dvdrental",                # database
-}
 
-obj1 = DB(db_config)
-rows = obj1.query('''SELECT * FROM actor LIMIT 5 OFFSET 2''')
+
+obj1 = DB(config)
+
+query = ''' 
+SELECT HUMANS.id, HUMANS.begin_name, HUMANS.end_name, \
+       BOOKS.id, BOOKS.author, BOOKS.name, \
+       READ_RETURN.id_user, READ_RETURN.id_book, READ_RETURN.is_returned
+FROM ((HUMAN
+INNER JOIN READ_RETURN ON HUMANS.id = READ_RETURN.id_user)
+INNER JOIN BOOKS ON BOOKS.id = READ_RETURN.id_book);
+'''
+rows = obj1.get_data(query)
 for item in rows:
     print(item)
+query2 = '''     
+obj.count_frequency(query2)
+'''
+
+
+Write code for next requirements:
+  2. Write class for getting data from tables
+  3. Write class for counting how often any human has read each book
