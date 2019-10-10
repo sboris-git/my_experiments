d59a26abb0f7fc657df6684594ece5e593082446
Boris Skip <boris.v.skip@gmail.com>
2019-10-10 15:43:32 +0300

    Sanity clean up

--- PostgresDB/Tutorials/test_1.py
+++ PostgresDB/Tutorials/test_1.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect(database="testdb", user = "postgres", password = "SBoris", host = "127.0.0.1", port = "5432")
+conn = psycopg2.connect(database="testdb", user = "postgres", password = "*****", host = "127.0.0.1", port = "5432")
 
 print("Opened database successfully")
 
