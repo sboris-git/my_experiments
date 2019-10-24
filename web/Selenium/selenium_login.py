7f0f8405bdff03f05f3733fb650c243ef9a595d5
Boris Skip <boris.v.skip@gmail.com>
2019-10-24 15:45:38 +0300

    In progress - assertion added for log in with Selenium

--- web/Selenium/selenium_login.py
+++ web/Selenium/selenium_login.py
@@ -5,7 +5,8 @@ username = 'username' # login
 paswd = "********"
 url = 'https://www.linkedin.com/login'
 pause = 2  # sec
-driver = webdriver.Chrome('/home/stable/Apps/chromedriver')
+# driver = webdriver.Chrome('/home/stable/Apps/chromedriver')
+driver = webdriver.Chrome('/home/boris/Apps/')
 
 driver.get(url)
 print("Locating and fill in username")
@@ -23,5 +24,10 @@ xpath = '//button[contains(@aria-label,"Sign in")]'
 driver.find_element_by_xpath(xpath).click()
 time.sleep(pause*2)
 
+actualUrl = "https://www.linkedin.com/feed/"
+expectedUrl = driver.getCurrentUrl()
+# Assert.assertEquals(expectedUrl, actualUrl)
+Assert.assertEquals('Url', 'Url')
+print(Assert.assertEquals('Url', 'Url'))
 
 driver.quit()
