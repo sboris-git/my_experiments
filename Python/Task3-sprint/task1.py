1e19894d3710c70ce415ea0d7c0fe8b9f0f79ebe
Boris Skip <boris.v.skip@gmail>
2019-12-01 22:03:37 +0200

    first_test_run

--- Python/Task3-sprint/task1.py
+++ Python/Task3-sprint/task1.py
@@ -1,16 +1,17 @@
 from modul import list_to_str
 
 
-def filter_7_5():
+def filter_7_5(a, b):
 
-    list_a = [number for number in range(2000, 3201)]
+    list_a = [number for number in range(a, b + 1)]
 
     filter_obj = filter(lambda x: x % 7 == 0, filter(lambda x: x % 5 != 0, list_a))
     string = list_to_str(filter_obj, ', ')
 
     return string
 
+
 # list_a = [7, 15, 14, 4, 5, 21, 35, 49]
-print(filter_7_5())
+print(filter_7_5(2000, 3200))
 
 
