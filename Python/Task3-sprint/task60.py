6d90b20ee6dba2d8f91f9ec9819d8f0e71bda346
Boris Skip <boris.v.skip@gmail.com>
2019-12-02 13:50:47 +0200

    sprint in progress

--- Python/Task3-sprint/task60.py
+++ Python/Task3-sprint/task60.py
@@ -1,7 +1,10 @@
 def text_to_unicode(text):
     '''into byte!!!! b'hello word' - not ToDo unicode - have a look at task82.py'''
+    # in Python 3 str type is Unicode
 
     text = text.encode('utf-8') #  into byte!!!! not ToDo unicode - have a look at task82.py
+    text = text.decode('utf-8')
+
     return text
 
 
